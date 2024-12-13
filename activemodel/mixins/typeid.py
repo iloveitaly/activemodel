c7445c7cc0ba1bcad502c26b06f5250b91228176
Michael Bianco <iloveitaly@gmail.com>
2024-12-13 05:47:11 -0700

    refactor: remove unused TypeIDMixin2 class definition
    Generated-by: aiautocommit

--- activemodel/mixins/typeid.py
+++ activemodel/mixins/typeid.py
@@ -16,21 +16,23 @@ def TypeIDMixin(prefix: str):
     return _TypeIDMixin
 
 
-class TypeIDMixin2:
-    """
-    Mixin class that adds a TypeID primary key to models.
+# TODO not sure if I love the idea of a dynamic class for each mixin as used above
+#      may give this approach another shot in the future
+# class TypeIDMixin2:
+#     """
+#     Mixin class that adds a TypeID primary key to models.
 
 
-    >>>    class MyModel(BaseModel, TypeIDMixin, prefix="xyz", table=True):
-    >>>        name: str
+#     >>>    class MyModel(BaseModel, TypeIDMixin, prefix="xyz", table=True):
+#     >>>        name: str
 
-    Will automatically have an `id` field with prefix "xyz"
-    """
+#     Will automatically have an `id` field with prefix "xyz"
+#     """
 
-    def __init_subclass__(cls, *, prefix: str, **kwargs):
-        super().__init_subclass__(**kwargs)
+#     def __init_subclass__(cls, *, prefix: str, **kwargs):
+#         super().__init_subclass__(**kwargs)
 
-        cls.id: uuid.UUID = Field(
-            sa_column=Column(TypeIDType(prefix), primary_key=True),
-            default_factory=lambda: TypeID(prefix),
-        )
+#         cls.id: uuid.UUID = Field(
+#             sa_column=Column(TypeIDType(prefix), primary_key=True),
+#             default_factory=lambda: TypeID(prefix),
+#         )
