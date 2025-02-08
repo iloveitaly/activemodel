ff777d127adc63f5730e734f08346d3a0ab24c1d
Michael Bianco <iloveitaly@gmail.com>
2025-02-08 08:48:55 -0700

    refactor: include field descriptions in DB schema comments
    - Add field descriptions as comments in SQLAlchemy column definitions to enhance model documentation.
    - Update get_column_from_field to insert comments only if they are not already set.
    
    Generated-by: aiautocommit

--- activemodel/get_column_from_field_patch.py
+++ activemodel/get_column_from_field_patch.py
@@ -1,3 +1,13 @@
+"""
+Pydantic has a great DX for adding docstrings to fields. This allows devs to easily document the fields of a model.
+
+Making sure these docstrings make their way to the DB schema is helpful for a bunch of reasons (LLM understanding being one of them).
+
+This patch mutates a core sqlmodel function which translates pydantic FieldInfo objects into sqlalchemy Column objects. It adds the field description as a comment to the column.
+
+Note that FieldInfo *from pydantic* is used when a "bare" field is defined. This can be confusing, because when inspecting model fields, the class name looks exactly the same.
+"""
+
 from typing import (
     TYPE_CHECKING,
     Any,
@@ -22,11 +32,19 @@ from sqlmodel._compat import (  # type: ignore[attr-defined]
 )
 from sqlmodel.main import FieldInfo, get_sqlalchemy_type
 
+from activemodel.utils import hash_function_code
+
 if TYPE_CHECKING:
     from pydantic._internal._model_construction import ModelMetaclass as ModelMetaclass
     from pydantic._internal._repr import Representation as Representation
     from pydantic_core import PydanticUndefined as Undefined
     from pydantic_core import PydanticUndefinedType as UndefinedType
+
+
+assert (
+    hash_function_code(sqlmodel.main.get_column_from_field)
+    == "398006ef8fd8da191ca1a271ef25b6e135da0f400a80df2f29526d8674f9ec51"
+)
 
 
 def get_column_from_field(field: PydanticFieldInfo | FieldInfo) -> Column:  # type: ignore
@@ -39,11 +57,11 @@ def get_column_from_field(field: PydanticFieldInfo | FieldInfo) -> Column:  # ty
     else:
         field_info = field.field_info
 
-    # if isinstance(field_info, PydanticFieldInfo):
-    #     breakpoint()
-
     sa_column = getattr(field_info, "sa_column", Undefined)
     if isinstance(sa_column, Column):
+        # IMPORTANT: change from the original function
+        if not sa_column.comment and (field_comment := field_info.description):
+            sa_column.comment = field_comment
         return sa_column
 
     primary_key = getattr(field_info, "primary_key", Undefined)
@@ -97,15 +115,19 @@ def get_column_from_field(field: PydanticFieldInfo | FieldInfo) -> Column:  # ty
         args.extend(list(cast(Sequence[Any], sa_column_args)))
 
     sa_column_kwargs = getattr(field_info, "sa_column_kwargs", Undefined)
-    if sa_column_kwargs is not Undefined:
-        kwargs.update(cast(Dict[Any, Any], sa_column_kwargs))
 
-    # IMPORTANT: this is the only change from the original function
-    if isinstance(field_info, PydanticFieldInfo) and field_info.description:
+    # IMPORTANT: change from the original function
+    if field_info.description:
         if sa_column_kwargs is Undefined:
             sa_column_kwargs = {}
 
-        sa_column_kwargs["comment"] = field_info.description
+        assert isinstance(sa_column_kwargs, dict)
+
+        # only update comments if not already set
+        if "comment" not in sa_column_kwargs:
+            sa_column_kwargs["comment"] = field_info.description
+
+    if sa_column_kwargs is not Undefined:
         kwargs.update(cast(Dict[Any, Any], sa_column_kwargs))
 
     sa_type = get_sqlalchemy_type(field)
