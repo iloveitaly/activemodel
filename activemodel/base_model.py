b67e68a85050c96f1fc338f4529cbbb1f023071b
Michael Bianco <mike@mikebian.co>
2025-12-27 11:48:24 -0700

    refactor: move get_column_from_field_patch to patches package
    Generated-by: aiautocommit

--- activemodel/base_model.py
+++ activemodel/base_model.py
@@ -15,7 +15,7 @@ from sqlalchemy.orm import declared_attr
 from activemodel.mixins.pydantic_json import PydanticJSONMixin
 
 # NOTE: this patches a core method in sqlmodel to support db comments
-from . import get_column_from_field_patch  # noqa: F401
+from .patches import get_column_from_field_patch  # noqa: F401
 from .query_wrapper import QueryWrapper
 from .session_manager import get_session
 
