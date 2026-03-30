fac58ebf0829124de65d365d46745a794f43741c
Michael Bianco <mike@mikebian.co>
2026-03-30 12:15:03 -0600

    feat(model): implement soft delete logic in SoftDeletionMixin
    *   Implement the `soft_delete` method to timestamp records with the current UTC time instead of performing a hard deletion.
    *   Update `deleted_at` field definition to explicitly allow `None` values.
    *   Add a `SoftDeleteRecord` protocol to improve type safety for models utilizing this mixin.
    
    Generated-by: aiautocommit

--- activemodel/mixins/soft_delete.py
+++ activemodel/mixins/soft_delete.py
@@ -1,17 +1,33 @@
-from datetime import datetime
+from datetime import UTC, datetime
+from typing import Protocol, Self
 
 import sqlalchemy as sa
 from sqlmodel import Field
 
 
+class SoftDeleteRecord(Protocol):
+    deleted_at: datetime | None
+
+    def save(self) -> Self: ...
+
+
 class SoftDeletionMixin:
-    deleted_at: datetime = Field(
+    """
+    Soft delete records by setting `deleted_at` instead of removing the row.
+
+    Call `soft_delete()` to timestamp the record and persist that change.
+    """
+
+    deleted_at: datetime | None = Field(
         default=None,
         nullable=True,
         # TODO https://github.com/fastapi/sqlmodel/discussions/1228
         sa_type=sa.DateTime(timezone=True),  # type: ignore
     )
 
-    def soft_delete(self):
-        self.deleted_at = datetime.now()
-        raise NotImplementedError("Soft deletion is not implemented")
+    def soft_delete[T: SoftDeleteRecord](self: T) -> T:
+        """Timestamp `deleted_at` and persist the record."""
+
+        self.deleted_at = datetime.now(UTC)
+        # TODO we should limit the fields updated to just `deleted_at`
+        return self.save()
