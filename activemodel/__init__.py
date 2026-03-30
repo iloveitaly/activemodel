0d51eeb0403057294e0a0384cbd98e8114cbeaa3
Michael Bianco <mike@mikebian.co>
2026-03-30 12:57:36 -0600

    refactor: export public components in __init__
    Generated-by: aiautocommit

--- activemodel/__init__.py
+++ activemodel/__init__.py
@@ -2,3 +2,11 @@ from .base_model import BaseModel
 
 # from .field import Field
 from .session_manager import SessionManager, get_engine, get_session, init
+
+__all__ = [
+    "BaseModel",
+    "SessionManager",
+    "get_engine",
+    "get_session",
+    "init",
+]
